27e42a304ab76d477ba6ab9feeb6ee289753046e
Karantea <selke.josefine@proton.me>
2023-02-01 12:50:32 +0100

    Arrays, Dot product, One-Layer with NumPy

--- Chapter2.py
+++ Chapter2.py
@@ -7,8 +7,8 @@ if __name__ == "__main__":
     # 1 neuron (= 1 bias), 4 inputs
     # weights (trainable parameters to adjust) get assigned
     # randomly most times
-    inputs = [1,2,3, 2.5]
-    weights = [0.2, 0.8, -0.5, 1]
+    inputs = [1.0, 2.0, 3.0, 2.5]
+    weights = [0.2, 0.8, -0.5, 1.0]
     bias = 2
 
     # weighted sum [4.8]
@@ -48,10 +48,10 @@ if __name__ == "__main__":
     ]
 
     #automating code
-    weightsAll = [[0.2, 0.8, -0.5, 1],
+    weightsAll = [[0.2, 0.8, -0.5, 1.0],
                   [0.5, -0.91, 0.26, -0.5],
                   [-0.26, -0.27, 0.17, 0.87]]
-    biases = [2, 3, 0.5]
+    biases = [2.0, 3.0, 0.5]
     layers_output = []
 
     for neuron_weights, neuron_bias in zip(weightsAll, biases):
@@ -63,4 +63,21 @@ if __name__ == "__main__":
         neuron_output += neuron_bias
         layers_output.append(neuron_output)
 
-    # Tensors, Arrays, Vectors
+    #A single neuron with NumPy
+    outputs = np.dot(weights, inputs) + bias #4.8
+    #A layer of neurons with NumPy
+    layers_outputs = np.dot(weightsAll, inputs) + biases #[4.8   1.21  2.385]
+    #A batch of data - Matrix product
+    # second dimension of first matrix must fit first dimension
+    # of second matrix
+    a = [1, 2, 3]
+    b = [2, 3, 4]
+    a = np.array([a])
+    b = np.array([b]).T
+    print(np.dot(a, b)) #.dot() for dot and matrix product
+
+    input_batch = [[1.0, 2.0, 3.0, 2.5],
+                   [2.0, 5.0, -1.0, 2.0],
+                   [-1.5, 2.7, 3.3, -0.8]]
+    output_batch = np.dot(input_batch, np.array(weightsAll).T) + biases
+    print(output_batch)
